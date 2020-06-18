34a7585c374273504028c414ba81dd5b61aa5996
Justin Sandahl <sandahl.j.d@gmail.com>
2020-06-18 21:17:37 +0100

    refactored error handling to use state to render messages as an element when an error occurs, error message gets rendered along with an empty search box to allow another search.

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect } from "react";
-import Axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import LocationDetails from "./location-details";
 import ForecastList from "./forecast-list";
 
@@ -7,62 +7,93 @@ import "../styles/app.css";
 import ForecastDetails from "./forecast-details";
 import SearchForm from "./search-form";
 
-const App = props => {
+const App = (props) => {
   const [selectedDate, setSelectedDate] = useState(0);
   const [forecasts, setForecasts] = useState([]);
-  const [location, setLocation] = useState({city: '', country: '',});
+  const [location, setLocation] = useState({ city: "", country: "" });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    Axios.get('https://mcr-codes-weather.herokuapp.com/forecast')
-    .then((response) => {
-      setForecasts(response.data.forecasts);
-      setLocation(response.data.location);
-    });
+    setLoading(true);
+    setError(null);
+    Axios.get("https://mcr-codes-weather.herokuapp.com/forecast").then(
+      (response) => {
+        setForecasts(response.data.forecasts);
+        setLocation(response.data.location);
+        setLoading(false);
+      }
+    );
   }, []);
 
   const handleCitySearch = (city) => {
     const request = city.toLowerCase();
-    
-    Axios.get(`https://mcr-codes-weather.herokuapp.com/forecast?city=${request}`)
-    .then((response) => {
-      setForecasts(response.data.forecasts);
-      setLocation(response.data.location);
-    })
-    .catch((err) => {
-      if (err.response){
-      alert(`${err.message}: The city entered could not be found.`);
-      }
-      else if (err.request){
-        console.log('Error', err.message);
-      }
-      else {
-        console.log('Error', err.message);
-      }
-    });
+
+    setLoading(true);
+    setError(null);
+
+    Axios.get(
+      `https://mcr-codes-weather.herokuapp.com/forecast?city=${request}`
+    )
+      .then((response) => {
+        setForecasts(response.data.forecasts);
+        setLocation(response.data.location);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err.response) {
+          setLoading(false);
+          setError(`${err.message}: The city entered was not found`);
+        } else {
+          setLoading(false);
+          setError(err.message);
+        }
+      });
   };
 
-  const selectedForecast = forecasts.find(forecast => forecast.date === selectedDate);
-  
+  const selectedForecast = forecasts.find(
+    (forecast) => forecast.date === selectedDate
+  );
+
   const handleForecastSelect = (date) => {
     setSelectedDate(date);
   };
 
-  return (
-    <div className="forecast">
-      <LocationDetails
-        city={location.city}
-        country={location.country}
-      />
+  if (loading && !error) {
+    return (
+      <div className="forecast">
+        <h1 className="appLoading">Loading the weather...</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div className="forecast">
+        {error && (
+          <>
+            <h2 className="error-message">`{error}`</h2>
+            <SearchForm onCitySearch={handleCitySearch} />
+          </>
+        )}
 
-      <SearchForm onCitySearch={handleCitySearch}/>
+        {!loading && !error && (
+          <>
+            <LocationDetails city={location.city} country={location.country} />
+            
+            <SearchForm onCitySearch={handleCitySearch} />
+            
+            <ForecastList
+              forecasts={forecasts}
+              onForecastSelect={handleForecastSelect}
+            />
 
-      <ForecastList forecasts={forecasts} onForecastSelect={handleForecastSelect} />
-      {
-        selectedForecast && (<ForecastDetails forecasts={selectedForecast} />)
-      }
-    </div>
-  );
-}
-
+            {selectedForecast && (
+              <ForecastDetails forecasts={selectedForecast} />
+            )}
+          </>
+        )}
+      </div>
+    );
+  }
+};
 
 export default App;
