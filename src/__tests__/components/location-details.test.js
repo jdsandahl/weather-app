114e2a74752e0e9c77abf3901a826a26939cd1c3
Justin Sandahl <sandahl.j.d@gmail.com>
2020-06-20 20:55:53 +0100

    renamed test to better describe its function

--- src/__tests__/components/location-details.test.js
+++ src/__tests__/components/location-details.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import LocationDetails from '../../components/location-details';
 
-it('renders the correct city and location props', () => {
+it('renders correct className applied to the element containing city and location text', () => {
     const { getByText } = render (
         <LocationDetails city="Manchester" country="UK" />
     );
