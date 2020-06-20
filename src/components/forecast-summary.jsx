f6a4b229366e676b4e97bbb69b872fa824291682
Justin Sandahl <sandahl.j.d@gmail.com>
2020-06-20 21:58:16 +0100

    refactored using destructuring to clean up code.

--- src/components/forecast-summary.jsx
+++ src/components/forecast-summary.jsx
@@ -4,21 +4,21 @@ import PropTypes from "prop-types";
 import WeatherIcon from 'react-icons-weather';
 import moment from 'moment';
 
-const ForecastSummary = (props) => (
+const ForecastSummary = ({ date, icon, temperature, description, onSelect }) => (
   <div className="forecast-summary">
     <div className="date" data-testid="date-id">
-      <span>{moment(props.date).format('ddd Do MMM')}</span>
+      <span>{moment(date).format('ddd Do MMM')}</span>
     </div>
     <div className="icon" data-testid="icon-id">
-      <WeatherIcon name="owm" iconId={props.icon} />
+      <WeatherIcon name="owm" iconId={icon} />
     </div>
     <div className="temperature" data-testid="temperature-id">
-      <span>{props.temperature}&deg;c</span>
+      <span>{temperature}&deg;c</span>
     </div>
     <div className="description" data-testid="description-id">
-      <span>{props.description}</span>
+      <span>{description}</span>
     </div>
-    <button onClick={() => props.onSelect(props.date)}>More Details</button>
+    <button onClick={() => onSelect(date)}>More Details</button>
   </div>
 );
 
