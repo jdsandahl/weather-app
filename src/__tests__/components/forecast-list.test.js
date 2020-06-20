7bf73331ac473d86cb03f076d28cd6bb82a777d6
Justin Sandahl <sandahl.j.d@gmail.com>
2020-06-20 20:19:31 +0100

    added test to check that dates are being rendered in the correct format.

--- src/__tests__/components/forecast-list.test.js
+++ src/__tests__/components/forecast-list.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { cleanup, render } from '@testing-library/react';
 import ForecastList from '../../components/forecast-list';
+import moment from 'moment';
 
 afterEach(cleanup);
 
 describe("ForecastList", () => {
     const forecasts = [
         {
-            date: 123,
+            date: 123456789,
             description: "date1",
             icon: "200",
             temperature: {
@@ -37,5 +38,18 @@ describe("ForecastList", () => {
         expect(getAllByTestId("icon-id")).toHaveLength(2);
         expect(getAllByTestId("temperature-id")).toHaveLength(2);
         expect(getAllByTestId("description-id")).toHaveLength(2);
+    });
+
+    it("renders the correct dates and in the correct format", () => {
+        const { getAllByText } = render (<ForecastList forecasts={ forecasts } />);
+        
+        const testDate1 = moment(123456789).format('ddd Do MMM');
+        const testDate2 = moment(456).format('ddd Do MMM');
+
+        expect(getAllByText(testDate1)).toBeTruthy();
+        expect(getAllByText(testDate1)).toHaveLength(1);
+
+        expect(getAllByText(testDate2)).toBeTruthy();
+        expect(getAllByText(testDate2)).toHaveLength(1);
     });
 });
